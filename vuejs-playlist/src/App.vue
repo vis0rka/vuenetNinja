99bbc12e7c33ef7d729eca94e2d76bd2ea4ae4c3
Viktor <barta.viktor.ufs@gmail.com>
2019-05-21 16:54:11 +0200

    feat(dynamic form component) use slots to create 2 diff form and use keep-alive to keep data in form

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,51 +1,27 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is a title of the from</h3>
-        <p>Information about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" name="" placeholder="name" required>
-        <input type="password" name="" placeholder="password" required>
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+    <component :is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show form one </button>
+    <button v-on:click="component = 'form-two'">Show form two </button>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Ninjas from "./components/Ninjas";
 import formHelper from "./components/formHelper";
+import formOne from "./components/formOne";
+import formTwo from "./components/formTwo";
 
 export default {
   components: {
-    "app-header": Header,
-    "app-footer": Footer,
-    "app-ninjas": Ninjas,
-    "form-helper": formHelper
+    "form-one": formOne,
+    "form-two": formTwo
   },
   data() {
     return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ],
-      title: "I am a dynamic slot title"
+      component: 'form-two'
     };
-  },
-  methods: {
-    updateTitle(updatedTitle) {
-      this.title = updatedTitle;
-    }
   }
 };
 </script>
